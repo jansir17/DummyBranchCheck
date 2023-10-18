c5f933ed6503864abe392c27c84fc3e0577c4026
jansir71 <jansir71@protonmail.com>
2023-10-18 13:35:45 +0600

    updating from pc2, asus branch

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -2,8 +2,6 @@ public class HelloWorld
 {
  public static void main(String[] args)
 {
-   System.out.println("Hello Jansir Checking Branch! for second file ? ");
- System.out.println("updating file 2 to in Asus branch in github site, checking whether i can pull in asus branch");
- System.out.println("Tyring to change and pull the file2 from asus branch from github and pulling it on laptop. again");
+System.out.println("Updating from pc2, on Asus_branch");
 }
 }
