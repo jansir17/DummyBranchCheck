d5bc8803db78e138faa2dade3dda87f4942bb412
jansir71 <jansir71@protonmail.com>
2023-10-18 13:41:35 +0600

    Changing to check pull request

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -2,8 +2,6 @@ public class HelloWorld
 {
  public static void main(String[] args)
 {
-   System.out.println("Hello Jansir Checking Branch! for second file ? ");
- System.out.println("Since after updating from Asus branch from github, while pulling it did not pull So now updating from Main branch");
-   
+System.out.println("Changing from pc2, on main branch");
 }
 }
