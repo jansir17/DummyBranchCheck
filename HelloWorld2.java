93eca0cd92f6f7575db6075f11f6d9b765db7c70
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2023-10-18 13:09:20 +0600

    Update HelloWorld2.java

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -3,5 +3,7 @@ public class HelloWorld
  public static void main(String[] args)
 {
    System.out.println("Hello Jansir Checking Branch! for second file ? ");
+ System.out.println("Since after updating from Asus branch from github, while pulling it did not pull So now updating from Main branch");
+   
 }
 }
