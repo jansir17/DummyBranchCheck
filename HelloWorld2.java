7fb2939eb8523d67c688ec7e0e8706ed896e25ce
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2023-10-18 13:36:55 +0600

    Update HelloWorld2.java

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -3,5 +3,6 @@ public class HelloWorld
  public static void main(String[] args)
 {
 System.out.println("Updating from pc2, on Asus_branch");
+System.out.println("Updating from pc2, on Asus_branch");
 }
 }
